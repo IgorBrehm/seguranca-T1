20f4dc9dd63b67cb94b75dfb3b68ed6ec993bda6
IgorBrehm <igorbrehm@hotmail.com>
2020-04-05 21:56:01 -0300

    versao naive funcionando

--- Decriptador.java
+++ Decriptador.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Decriptador {
 
-    public String textoCifrado;
+    public char[] textoCifrado;
     public double coincidencia = 0.072723; // indice de coincidencia para o portugues
     public ArrayList<String> letras = new ArrayList<String>();
     public ArrayList<Double> frequencias = new ArrayList<Double>();
@@ -15,34 +15,93 @@ public class Decriptador {
         lerArquivos(caminho);
 
         int tamanhoChave = encontrarTamanhoChave();
-        //int[] deslocamentos = encontrarDeslocamentos(tamanhoChave);
 
-        //mostrarTextoDecifrado(deslocamentos);
+        int[] deslocamentos = encontrarDeslocamentos(tamanhoChave);
+
+        mostrarTextoDecifrado(deslocamentos);
     }
 
+    // Usa as letras da chave para decifrar o texto de entrada e mostrar o texto claro
     public void mostrarTextoDecifrado(int[] deslocamentos){
         
-        System.out.println();
+        int min = 65; // A em ASCII
+        int max = 90; // Z em ASCII
+        int indice = 0;
+
+        for(int g = 0; g < textoCifrado.length; g++){
+            int cifrado = (int)textoCifrado[g];
+            int deslocamento = deslocamentos[indice];
+            if((cifrado - deslocamento) < min){
+                int aux = min - (cifrado - deslocamento);
+                cifrado = (max - aux) + 1;
+                //System.out.println(textoCifrado[g]);
+                //System.out.println(deslocamento);
+                //System.out.println((char)cifrado);
+            }
+            else{
+                cifrado = cifrado - deslocamento; 
+            }
+
+            indice += 1;
+            if(indice == deslocamentos.length){
+                indice = 0;
+            }
+            System.out.print((char)cifrado);
+        }     
     }
 
+    // Usa o tamanho da chave para descobrir as letras
     public int[] encontrarDeslocamentos(int tamanhoChave){
 
-        return new int[5];
+        int[] resposta = new int[tamanhoChave];
+        for(int w = 0; w < tamanhoChave; w++){ // controlando a quantia de iteracoes em relacao a i
+
+            int[] ocorrencias = new int[letras.size()];
+            for(int j = w; j < textoCifrado.length; j += tamanhoChave) { // passando por todos os elementos
+                for(int k = 0; k < letras.size(); k++){ // comparando o elemento atual com as letras do alfabeto
+                    char aux = letras.get(k).charAt(0);
+                    //System.out.println(array[j]+" : "+aux+" ==? "+(array[j] == aux));
+                    if(aux == textoCifrado[j]){
+                        ocorrencias[k] += 1;
+                        break;
+                    }
+                }
+            }
+            
+            // sei as ocorrencias de cada letra
+            // a letra que mais apareceu vai ser a letra usada para cifrar o A
+            int maior = 0;
+            int indice = 0;
+            for(int p = 0; p < ocorrencias.length; p++){
+                if(ocorrencias[p] > maior){
+                    maior = ocorrencias[p]; 
+                    indice = p;
+                }
+            }
+            // sei o indice da lista de letras onde tiveram mais ocorrencias
+            // verifico o deslocamento entre a letra e o A
+            int senha = (int)letras.get(indice).charAt(0);
+            int A = (int)letras.get(0).charAt(0);
+            resposta[w] = senha - A;
+            System.out.println("Senha: "+ senha+" = "+(char)senha);
+            System.out.println("Deslocamento "+ (w+1) +": "+resposta[w]);
+            //Thread.sleep(1000);
+        }
+        return resposta;
     }
 
+    // Encontra o tamanho da chave usada para cifrar o texto
     public int encontrarTamanhoChave() {
-
-        char[] array = textoCifrado.toCharArray();
 
         for(int i = 1; i <= 10; i++){ // pulando de i em i elementos ate pular de 10 em 10
             for(int w = 0; w < i; w++){ // controlando a quantia de iteracoes em relacao a i
 
                 int[] ocorrencias = new int[letras.size()];
-                for(int j = w; j < array.length; j += i) { // passando por todos os elementos
+                for(int j = w; j < textoCifrado.length; j += i) { // passando por todos os elementos
                     for(int k = 0; k < letras.size(); k++){ // comparando o elemento atual com as letras do alfabeto
                         char aux = letras.get(k).charAt(0);
                         //System.out.println(array[j]+" : "+aux+" ==? "+(array[j] == aux));
-                        if(aux == array[j]){
+                        if(aux == textoCifrado[j]){
                             ocorrencias[k] += 1;
                             break;
                         }
@@ -61,7 +120,7 @@ public class Decriptador {
                 //System.out.println(diff + " : " + (Double.compare(diff, 0.00999) < 0));
                 //Thread.sleep(1000);
                 if(Double.compare(diff, 0.00999) < 0){
-                    //System.out.println(i); // tamanho da chave encontrado
+                    System.out.println("Tamanho da chave encontrado: "+i); // tamanho da chave encontrado
                     return i;
                 }
             }
@@ -75,16 +134,11 @@ public class Decriptador {
         File arquivoEntrada = new File(caminho);
         Scanner in = new Scanner(arquivoEntrada);
 
-        /*
-        String[] entrada = in.nextLine().split(":");
-        System.out.println("Texto Claro: "+entrada[0].toUpperCase());
-        System.out.println("Chave a ser encontrada: "+entrada[1].toUpperCase());
-        System.out.println("Texto cifrado: "+entrada[2].toUpperCase());
-        textoCifrado = entrada[2].toUpperCase();
-        */
-        textoCifrado = in.nextLine().toUpperCase();
+        String texto = in.nextLine().toUpperCase();
+        textoCifrado = texto.toCharArray();
+        System.out.println("Texto cifrado: "+texto);
 
-        File arquivoFrequencias = new File("frequencias.txt");
+        File arquivoFrequencias = new File("frequencias.txt"); // arquivo com as letras e suas frequencias no portugues
         in.close();
         in = new Scanner(arquivoFrequencias);
 
